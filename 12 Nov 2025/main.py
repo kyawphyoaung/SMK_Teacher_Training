c02cfaf0fd354940968817188218a5e61b78b3fa
Kyaw Phyo Aung <aprilnikee@gmail.com>
2025-11-13 15:55:02 +0630

    12 Nov 2025 course update

---
+++ 12 Nov 2025/main.py
@@ -0,0 +1,7 @@
+firstNum = int(input("Please enter the first number."))
+secondNum = int(input("Please enter the second number."))
+if secondNum == 0:
+    print("Second number can't be zero.")
+else:
+    result = firstNum / scondNum
+    print(result)
